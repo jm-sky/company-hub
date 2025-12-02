b3a07bf1cee4933a984997b1224f525497c69c74
Jan Madeyski <jan.madeyski@gmail.com>
2025-12-02 10:11:08 +0000

    fixes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,7 +4,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  /* config options here */
 };
 
 export default withNextIntl(nextConfig);
